6e157e5d194266075f565e4e09bb86f99c7144ce
zerocool <one1970zero@mail.ru>
2024-02-03 22:43:03 +0300

    add new fragments

--- app/src/main/java/ru/agavrilyuk/shop/MainActivity.kt
+++ app/src/main/java/ru/agavrilyuk/shop/MainActivity.kt
@@ -1,18 +1,27 @@
 package ru.agavrilyuk.shop
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import ru.agavrilyuk.shop.databinding.ActivityMainBinding
 import ru.agavrilyuk.shop.fragments.RegistrationFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RegistrationFragment.OnButtonClickInFragment {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragment_container, RegistrationFragment.newInstance())
             .commit()
+
+        binding.bottomNavigationView.visibility = View.GONE
     }
 
-    
+    override fun onClickButtonInRegFragment() {
+        binding.bottomNavigationView.visibility = View.VISIBLE
+    }
 }
--- app/src/main/java/ru/agavrilyuk/shop/fragments/RegistrationFragment.kt
+++ app/src/main/java/ru/agavrilyuk/shop/fragments/RegistrationFragment.kt
@@ -1,15 +1,15 @@
 package ru.agavrilyuk.shop.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.agavrilyuk.shop.MainActivity
+import ru.agavrilyuk.shop.R
 import ru.agavrilyuk.shop.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
+    private var onButtonClick: OnButtonClickInFragment? = null
     private var _binding: FragmentRegistrationBinding? = null
     private val binding get() = _binding ?: throw IllegalStateException("Binding is null")
 
@@ -24,10 +24,16 @@ class RegistrationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onButtonClick = activity as OnButtonClickInFragment
         binding.btEnter.setOnClickListener {
-            val intent = Intent(activity, MainActivity::class.java)
-            startActivity(intent)
+            requireActivity().supportFragmentManager
+                .beginTransaction().apply {
+                    replace(R.id.fragment_container, MainFragment())
+                }.commit()
+
+            onButtonClick?.onClickButtonInRegFragment()
         }
+
     }
 
     companion object {
@@ -38,5 +44,9 @@ class RegistrationFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    interface OnButtonClickInFragment {
+        fun onClickButtonInRegFragment()
     }
 }
